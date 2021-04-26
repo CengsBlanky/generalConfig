9e78cae400c7078af25d472425cc27be1768ea80
zengshuai <zengs1994@gmail.com>
2021-04-26 22:11:37 +0800

    add emacs config and zshrc

---
+++ .emacs.d/elpa/linum-relative-0.6/linum-relative-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from linum-relative.el  -*- no-byte-compile: t -*-
+(define-package "linum-relative" "0.6" "display relative line number in emacs." 'nil :commit "896df4b40c1e1eb59f55fcee48a1543f0ccd724e" :authors '(("coldnew" . "coldnew.tw@gmail.com")) :maintainer '("coldnew" . "coldnew.tw@gmail.com") :keywords '("converience") :url "http://github.com/coldnew/linum-relative")
---
+++ .emacs.d/elpa/slime-volleyball-1.1.7/slime-volleyball-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from slime-volleyball.el  -*- no-byte-compile: t -*-
+(define-package "slime-volleyball" "1.1.7" "An SVG Slime Volleyball Game" '((cl-lib "0.5")) :url "http://elpa.gnu.org/packages/slime-volleyball.html" :keywords '("games") :authors '(("Thomas Fitzsimmons" . "fitzsim@fitzsim.org")) :maintainer '("Thomas Fitzsimmons" . "fitzsim@fitzsim.org"))
