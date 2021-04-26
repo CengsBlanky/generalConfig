a07adb287e9c74566193c044a0ede9e476bbfbdb
zengshuai <zengs1994@gmail.com>
2021-04-26 22:16:13 +0800

    modify emacs config file

--- .emacs.d/elpa/slime-volleyball-1.1.7/slime-volleyball-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from slime-volleyball.el  -*- no-byte-compile: t -*-
-(define-package "slime-volleyball" "1.1.7" "An SVG Slime Volleyball Game" '((cl-lib "0.5")) :url "http://elpa.gnu.org/packages/slime-volleyball.html" :keywords '("games") :authors '(("Thomas Fitzsimmons" . "fitzsim@fitzsim.org")) :maintainer '("Thomas Fitzsimmons" . "fitzsim@fitzsim.org"))
